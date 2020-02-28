27036230b552d098d98c73cb4fa696228e0f1a97
kumarvaibhav45 <wave.vaibhav@gmail.com>
2020-02-28 19:09:33 +0530

    added options in the form

--- src/components/AddTransaction/index.jsx
+++ src/components/AddTransaction/index.jsx
@@ -3,50 +3,93 @@ import { GlobalState } from '../../context/GlobalState';
 
 const AddTransaction = () => {
   const [name, setName] = useState('');
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState('0');
+  const [hideForm, setHideForm] = useState(false);
+  const [moneyType, setMoneyType] = useState(0);
+  const [inputError, setInputError] = useState('');
   const { addTransaction } = useContext(GlobalState);
   const onSubmit = e => {
     e.preventDefault();
+    if (moneyType === 0 || moneyType === '0') {
+      setInputError('please select type of money!');
+      return;
+    }
+    let amt = amount;
+    if (amt.charAt(0) === '-') {
+      amt = -amt;
+    }
+    if (moneyType === 1) {
+      amt = +amt;
+    } else if (moneyType === 2) {
+      amt = -amt;
+    }
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       name,
-      amount: +amount
+      amount: +amt
     };
     addTransaction(newTransaction);
     setName('');
     setAmount('');
+    setInputError('');
   };
+
   return (
     <>
-      <h3>Add new transaction</h3>
-      <form onSubmit={onSubmit}>
-        <div className='form-control'>
-          <label htmlFor='name'>Text</label>
-          <input
-            required
-            type='text'
-            value={name}
-            placeholder='Enter Name...'
-            onChange={({ target: { value } }) => setName(value)}
-          />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='amount'>
-            Amount <br />
-            <small>(negative - expense, positive + income)</small>
-          </label>
-          <input
-            required
-            type='number'
-            value={amount}
-            placeholder='Enter Amount...'
-            onChange={({ target: { value } }) => setAmount(value)}
-          />
-        </div>
-        <button type='submit' className='btn'>
-          add transaction
-        </button>
-      </form>
+      <button
+        className='btn hide-btn'
+        onClick={() => setHideForm(prevState => !prevState)}
+      >
+        {hideForm ? 'show' : 'hide'} form
+      </button>
+      {!hideForm && (
+        <>
+          <h3>Add new transaction</h3>
+          <form onSubmit={onSubmit}>
+            <div className='form-control'>
+              <label htmlFor='name'>Name</label>
+              <input
+                required
+                type='text'
+                value={name}
+                placeholder='Enter Name...'
+                onChange={({ target: { value } }) => setName(value)}
+              />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='amount'>
+                Amount <small>(numbers only)</small>
+              </label>
+              <input
+                required
+                type='number'
+                value={amount}
+                placeholder='Enter Amount...'
+                onChange={({ target: { value } }) => setAmount(value)}
+              />
+            </div>
+            <div className='form-control'>
+              <select
+                required
+                value={moneyType}
+                onChange={({ target: { selectedIndex } }) =>
+                  setMoneyType(selectedIndex)
+                }
+              >
+                <option value='0' disabled>
+                  Select Type:
+                </option>
+                <option value='1'>Income</option>
+                <option value='2'>Expense</option>
+              </select>
+            </div>
+            <small className='error-msg'>{inputError}</small>
+            <button type='submit' className='btn'>
+              add transaction
+            </button>
+          </form>
+        </>
+      )}
     </>
   );
 };
